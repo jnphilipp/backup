3561ea4c3f6a76a33bbe0021e56d656e4811f89b
jnphilipp <me@jnphilipp.org>
2015-07-19 23:47:29 +0200

    Updated psync to support psync.xsd.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get install python3-gi rsync tar
 
 sudo cp psync /usr/bin/psync
+sudo cp psync.xsd /usr/share/psync/psync.xsd
 sudo chmod +x /usr/bin/psync
 
 MESSAGE="psync install completed."
